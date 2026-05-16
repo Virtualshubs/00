73c9e5047e9f0ce4be902c3399c1f49b7dc9df92
Virtualshubs <mmateriaa@gmail.com>
2026-05-16 12:52:52 +0200

    Add script loader for viewer-obfuscated.js

---
+++ loader.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  const script = document.createElement("script");
+  script.src = "viewer-obfuscated.js";
+
+  document.body.appendChild(script);
+
+});
