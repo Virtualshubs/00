b237677d1392b30b54c8b0130a1eee3dda81fb6a
Virtualshubs <mmateriaa@gmail.com>
2026-05-16 21:56:04 +0200

    Delete loader.js

--- loader.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-
-  const script = document.createElement("script");
-  script.src = "https://cdn.3dtwins.tech/visor/visor.js";
-
-  document.body.appendChild(script);
-
-});
